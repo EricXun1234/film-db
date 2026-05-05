ab66b17ecbfd5c17adeb9bf85856f4fd09cc3f0d
Eric <eric412529@gmail.com>
2026-05-05 16:35:04 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+let movies = [];
+
+fetch('movies.json')
+  .then(res => res.json())
+  .then(data => {
+    movies = data;
+  });
+
+function searchMovie() {
+  let keyword = document.getElementById("search").value.trim();
+  let resultsDiv = document.getElementById("results");
+
+  resultsDiv.innerHTML = "";
+
+  let filtered = movies.filter(movie =>
+    movie.title.includes(keyword) ||
+    movie.desc.includes(keyword) ||
+    movie.genres.some(c => c.includes(keyword)) ||
+    movie.scenes.some(s => s.includes(keyword)) ||
+    movie.moods.some(m => m.includes(keyword))
+  );
+
+
+  if (filtered.length === 0) {
+    resultsDiv.innerHTML = "<p style='color:#aaa;'>找不到相關電影 😢</p>";
+  }
+
+  else {
+    filtered.forEach(movie => {
+     movie.match_score = movie.match_score || Math.floor(Math.random() * 20) + 80;
+
+    movie.ai_reason = movie.ai_reason || "本系統根據關鍵字分析，判定此片段與搜尋需求高度相關。";
+      resultsDiv.innerHTML += `
+    <div class="card">
+
+      <img src="https://img.youtube.com/vi/${movie.ytId}/0.jpg" class="poster">
+
+      <h2>${movie.title}</h2>
+
+      <p class="meta">
+        類型：
+        ${movie.genres.map(c => `<span class="tag">${c}</span>`).join("")}
+        ｜ 年份：2011
+      </p>
+
+      <p class="reason">
+        ${movie.ai_reason}
+      </p>
+
+      <div class="info-box">
+
+        <div>
+          <div class="label">關鍵字</div>
+          <div>${[...movie.scenes, ...movie.moods].join("、")}</div>
+        </div>
+
+        <div>
+          <div class="label">匹配度</div>
+          <div class="score-bar">
+            <div style="width:${movie.match_score}%"></div>
+          </div>
+          <div>${movie.match_score}%</div>
+        </div>
+
+        <div>
+          <div class="label">類型</div>
+          <div>${movie.genres.join(" / ")}</div>
+        </div>
+
+      </div>
+
+      <button onclick="window.location.href='player.html?video=${movie.ytId}&title=${encodeURIComponent(movie.title)}'">
+  ▶ 播放
+</button>
+
+    </div>
+    `;
+    });
+  }
+  
+}
